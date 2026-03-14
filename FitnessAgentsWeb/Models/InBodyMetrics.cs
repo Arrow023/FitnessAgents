d59b9695b6c8ab2c310d4013efe8ac038d4e8037
arrow023 <piyushchohan48@gmail.com>
2026-03-14 17:26:34 +0530

    Add project files.

---
+++ FitnessAgentsWeb/Models/InBodyMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FitnessAgentsWeb.Models
+{
+    public class InBodyMetrics
+    {
+        [JsonPropertyName("skeletalMuscleMassKg")]
+        public double SkeletalMuscleMassKg { get; init; }
+
+        [JsonPropertyName("percentBodyFat")]
+        public double PercentBodyFat { get; init; }
+
+        [JsonPropertyName("basalMetabolicRate")]
+        public int BasalMetabolicRate { get; init; }
+    }
+}
---
+++ Terminal/DailyHealthMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Terminal
+{
+    public class DailyHealthMetrics
+    {
+        [JsonPropertyName("totalSleepMinutes")]
+        public int TotalSleepMinutes { get; init; }
+
+        [JsonPropertyName("averageRestingHeartRate")]
+        public int AverageRestingHeartRate { get; init; }
+
+        [JsonPropertyName("heartRateVariability")]
+        public double HeartRateVariability { get; init; }
+    }
+}
